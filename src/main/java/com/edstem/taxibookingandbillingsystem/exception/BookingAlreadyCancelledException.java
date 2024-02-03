90385801972b9664fe5a789b225760d0dae9d5c8
Sharok N <Sharukhahamed23@gmail.com>
2024-02-03 23:08:32 +0530

    Postman collection added

--- src/main/java/com/edstem/taxibookingandbillingsystem/exception/BookingAlreadyCancelledException.java
+++ src/main/java/com/edstem/taxibookingandbillingsystem/exception/BookingAlreadyCancelledException.java
@@ -3,7 +3,7 @@ package com.edstem.taxibookingandbillingsystem.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+@ResponseStatus(HttpStatus.ALREADY_REPORTED)
 public class BookingAlreadyCancelledException extends RuntimeException {
     public BookingAlreadyCancelledException() {
         super("Booking already cancelled");
