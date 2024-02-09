1ffe96bba3842179542b67af31248fa581f93765
Sharok N <Sharukhahamed23@gmail.com>
2024-02-09 11:26:59 +0530

    fixed errors

--- src/main/java/com/edstem/taxibookingandbillingsystem/exception/CustomGlobalExceptionHandler.java
+++ src/main/java/com/edstem/taxibookingandbillingsystem/exception/CustomGlobalExceptionHandler.java
@@ -2,14 +2,13 @@ package com.edstem.taxibookingandbillingsystem.exception;
 
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
+import java.util.Set;
+import java.util.stream.Collectors;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class CustomGlobalExceptionHandler {
--- src/main/java/com/edstem/taxibookingandbillingsystem/exception/InvalidUserException.java
+++ src/main/java/com/edstem/taxibookingandbillingsystem/exception/InvalidUserException.java
@@ -3,6 +3,7 @@ package com.edstem.taxibookingandbillingsystem.exception;
 public class InvalidUserException extends RuntimeException {
     private String entity;
     private Long id;
+
     public InvalidUserException(String entity) {
         super("Invalid " + entity);
     }
